838d939a2f5e1655931eddcf7b78a164e146a10b
Abe M <abe.malla8@gmail.com>
2024-10-26 03:07:55 -0700

    Refactors

--- Sources/CodeEditTextView/TextView/TextView+ItemBox.swift
+++ Sources/CodeEditTextView/TextView/TextView+ItemBox.swift
@@ -9,11 +9,16 @@ import AppKit
 import SwiftUI
 import LanguageServerProtocol
 
+/// Represents an item that can be displayed in the ItemBox
 public protocol ItemBoxEntry {
     var view: NSView { get }
 }
 
+private let WINDOW_PADDING: CGFloat = 5
+
 public final class ItemBoxWindowController: NSWindowController {
+
+    // MARK: - Properties
 
     /// Default size of the window when opened
     public static let DEFAULT_SIZE = NSSize(width: 300, height: 212)
@@ -23,54 +28,27 @@ public final class ItemBoxWindowController: NSWindowController {
         didSet { updateItems() }
     }
 
-    private let tableView = NSTableView()
-    private let scrollView = NSScrollView()
-    /// An event monitor for keyboard events
-    private var localEventMonitor: Any?
-
     /// Whether the ItemBox window is visbile
     public var isVisible: Bool {
         window?.isVisible ?? false
     }
 
+    // MARK: - Private Properties
+
+    private let tableView = NSTableView()
+    private let scrollView = NSScrollView()
+
+    /// An event monitor for keyboard events
+    private var localEventMonitor: Any?
+
+    // MARK: - Initialization
+
     public init() {
-        let window = NSWindow(
-            contentRect: NSRect(origin: CGPoint.zero, size: ItemBoxWindowController.DEFAULT_SIZE),
-            styleMask: [.resizable, .fullSizeContentView, .nonactivatingPanel],
-            backing: .buffered,
-            defer: false
-        )
-
-        // Style window
-        window.titleVisibility = .hidden
-        window.titlebarAppearsTransparent = true
-        window.isExcludedFromWindowsMenu = true
-        window.isReleasedWhenClosed = false
-        window.level = .popUpMenu
-        window.hasShadow = true
-        window.isOpaque = false
-        window.tabbingMode = .disallowed
-        window.hidesOnDeactivate = true
-        window.backgroundColor = .clear
-        window.minSize = ItemBoxWindowController.DEFAULT_SIZE
-
-        // Style the content with custom borders and colors
-        window.contentView?.wantsLayer = true
-        window.contentView?.layer?.backgroundColor = CGColor(
-            srgbRed: 31.0 / 255.0, green: 31.0 / 255.0, blue: 36.0 / 255.0, alpha: 1.0
-        )
-        window.contentView?.layer?.cornerRadius = 8.5
-        window.contentView?.layer?.borderWidth = 1
-        window.contentView?.layer?.borderColor = NSColor.gray.withAlphaComponent(0.45).cgColor
-        let innerShadow = NSShadow()
-        innerShadow.shadowColor = NSColor.black.withAlphaComponent(0.1)
-        innerShadow.shadowOffset = NSSize(width: 0, height: -1)
-        innerShadow.shadowBlurRadius = 2
-        window.contentView?.shadow = innerShadow
-
+        let window = Self.makeWindow()
         super.init(window: window)
 
-        setupTableView()
+        configureTableView(tableView)
+        configureScrollView(scrollView)
     }
 
     required init?(coder: NSCoder) {
@@ -85,18 +63,18 @@ public final class ItemBoxWindowController: NSWindowController {
 
     /// Opens the window as a child of another window
     public func showWindow(attachedTo parentWindow: NSWindow) {
-        guard let window = self.window else { return }
+        guard let window = window else { return }
+
         parentWindow.addChildWindow(window, ordered: .above)
         window.orderFront(nil)
 
         // Close on window switch
         NotificationCenter.default.addObserver(
-            forName: NSWindow.didResignKeyNotification,
-            object: parentWindow,
-            queue: .current
-        ) { [weak self] _ in
-            self?.close()
-        }
+            self,
+            selector: #selector(parentWindowDidResignKey),
+            name: NSWindow.didResignKeyNotification,
+            object: parentWindow
+        )
 
         self.show()
     }
@@ -108,24 +86,76 @@ public final class ItemBoxWindowController: NSWindowController {
         super.close()
     }
 
-    private func setupTableView() {
+    // MARK: - Private Methods
+
+    private static func makeWindow() -> NSWindow {
+        let window = NSWindow(
+            contentRect: NSRect(origin: .zero, size: ItemBoxWindowController.DEFAULT_SIZE),
+            styleMask: [.resizable, .fullSizeContentView, .nonactivatingPanel],
+            backing: .buffered,
+            defer: false
+        )
+
+        configureWindow(window)
+        configureWindowContent(window)
+        return window
+    }
+
+    private static func configureWindow(_ window: NSWindow) {
+        window.titleVisibility = .hidden
+        window.titlebarAppearsTransparent = true
+        window.isExcludedFromWindowsMenu = true
+        window.isReleasedWhenClosed = false
+        window.level = .popUpMenu
+        window.hasShadow = true
+        window.isOpaque = false
+        window.tabbingMode = .disallowed
+        window.hidesOnDeactivate = true
+        window.backgroundColor = .clear
+        window.minSize = ItemBoxWindowController.DEFAULT_SIZE
+    }
+
+    private static func configureWindowContent(_ window: NSWindow) {
+        guard let contentView = window.contentView else { return }
+
+        contentView.wantsLayer = true
+        contentView.layer?.backgroundColor = CGColor(
+            srgbRed: 31.0 / 255.0,
+            green: 31.0 / 255.0,
+            blue: 36.0 / 255.0,
+            alpha: 1.0
+        )
+        contentView.layer?.cornerRadius = 8.5
+        contentView.layer?.borderWidth = 1
+        contentView.layer?.borderColor = NSColor.gray.withAlphaComponent(0.45).cgColor
+
+        let innerShadow = NSShadow()
+        innerShadow.shadowColor = NSColor.black.withAlphaComponent(0.1)
+        innerShadow.shadowOffset = NSSize(width: 0, height: -1)
+        innerShadow.shadowBlurRadius = 2
+        contentView.shadow = innerShadow
+    }
+
+    private func configureTableView(_ tableView: NSTableView) {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.headerView = nil
         tableView.backgroundColor = .clear
-        tableView.intercellSpacing = NSSize.zero
+        tableView.intercellSpacing = .zero
         tableView.allowsEmptySelection = false
         tableView.selectionHighlightStyle = .regular
-        tableView.headerView = nil
         tableView.style = .plain
         tableView.usesAutomaticRowHeights = false
         tableView.rowSizeStyle = .custom
         tableView.rowHeight = 21
         tableView.gridStyleMask = []
+
         let column = NSTableColumn(identifier: NSUserInterfaceItemIdentifier("ItemsCell"))
         column.width = ItemBoxWindowController.DEFAULT_SIZE.width
         tableView.addTableColumn(column)
+    }
 
+    private func configureScrollView(_ scrollView: NSScrollView) {
         scrollView.documentView = tableView
         scrollView.hasVerticalScroller = true
         scrollView.verticalScroller = NoSlotScroller()
@@ -135,15 +165,21 @@ public final class ItemBoxWindowController: NSWindowController {
         scrollView.automaticallyAdjustsContentInsets = false
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.verticalScrollElasticity = .allowed
-        scrollView.contentInsets = NSEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
-        window?.contentView?.addSubview(scrollView)
+        scrollView.contentInsets = NSEdgeInsets(top: WINDOW_PADDING, left: 0, bottom: WINDOW_PADDING, right: 0)
+
+        guard let contentView = window?.contentView else { return }
+        contentView.addSubview(scrollView)
 
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: window!.contentView!.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: window!.contentView!.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: window!.contentView!.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: window!.contentView!.bottomAnchor)
+            scrollView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
+    }
+
+    @objc private func parentWindowDidResignKey() {
+        close()
     }
 
     private func updateItems() {
@@ -159,26 +195,26 @@ public final class ItemBoxWindowController: NSWindowController {
             switch event.type {
             case .keyDown:
                 switch event.keyCode {
-                case 53: // Escape key
+                case 53: // Escape
                     self.close()
-                case 125, 126: // Down Arrow and Up Arrow
+                    return nil
+                case 125, 126:  // Down/Up Arrow
                     self.tableView.keyDown(with: event)
                     return nil
-                case 36, 48: // Return and Tab key
+                case 36, 48:  // Return/Tab
                     return nil
                 default:
-                    break
+                    return event
                 }
             case .leftMouseDown, .rightMouseDown:
                 // If we click outside the window, close the window
                 if !NSMouseInRect(NSEvent.mouseLocation, self.window!.frame, false) {
                     self.close()
                 }
+                return event
             default:
-                break
+                return event
             }
-
-            return event
         }
     }
 
@@ -194,13 +230,11 @@ public final class ItemBoxWindowController: NSWindowController {
     }
 }
 
-extension ItemBoxWindowController: NSTableViewDataSource {
+extension ItemBoxWindowController: NSTableViewDataSource, NSTableViewDelegate {
     public func numberOfRows(in tableView: NSTableView) -> Int {
         return items.count
     }
-}
 
-extension ItemBoxWindowController: NSTableViewDelegate {
     public func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         items[row].view
     }
@@ -224,6 +258,7 @@ private class ItemBoxRowView: NSTableRowView {
         guard let context = NSGraphicsContext.current?.cgContext else { return }
 
         context.saveGState()
+        defer { context.restoreGState() }
 
         // Create a rect that's inset from the edges and has proper padding
         // TODO: We create a new selectionRect instead of using dirtyRect
@@ -231,20 +266,17 @@ private class ItemBoxRowView: NSTableRowView {
         // to select the first or last item that draws a clipped rectangular
         // selection highlight shape instead of the whole rectangle. Replace
         // this when it gets fixed.
-        let padding: CGFloat = 5
         let selectionRect = NSRect(
-            x: padding,
+            x: WINDOW_PADDING,
             y: 0,
-            width: bounds.width - (padding * 2),
+            width: bounds.width - (WINDOW_PADDING * 2),
             height: bounds.height
         )
-
         let cornerRadius: CGFloat = 5
         let path = NSBezierPath(roundedRect: selectionRect, xRadius: cornerRadius, yRadius: cornerRadius)
         let selectionColor = NSColor.gray.withAlphaComponent(0.19)
 
         context.setFillColor(selectionColor.cgColor)
         path.fill()
-        context.restoreGState()
     }
 }
