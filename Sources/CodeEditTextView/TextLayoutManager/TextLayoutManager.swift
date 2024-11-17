7456734b56522db87498fd8d89def8b37ac597c7
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2024-11-17 14:15:09 -0600

    Move All Delegate Calls Outside Layout Pass

--- Sources/CodeEditTextView/TextLayoutManager/TextLayoutManager.swift
+++ Sources/CodeEditTextView/TextLayoutManager/TextLayoutManager.swift
@@ -266,6 +266,13 @@ public class TextLayoutManager: NSObject {
         // Update the visible lines with the new set.
         visibleLineIds = newVisibleLines
 
+        #if DEBUG
+        isInLayout = false
+        #endif
+
+        // These are fine to update outside of `isInLayout` as our internal data structures are finalized at this point
+        // so laying out again won't break our line storage or visible line.
+
         if maxFoundLineWidth > maxLineWidth {
             maxLineWidth = maxFoundLineWidth
         }
@@ -274,15 +281,11 @@ public class TextLayoutManager: NSObject {
             delegate?.layoutManagerYAdjustment(yContentAdjustment)
         }
 
-        #if DEBUG
-        isInLayout = false
-        #endif
-        needsLayout = false
-
-        // This needs to happen after ``needsLayout`` is toggled. Things can be triggered by frame changes.
         if originalHeight != lineStorage.height || layoutView?.frame.size.height != lineStorage.height {
             delegate?.layoutManagerHeightDidUpdate(newHeight: lineStorage.height)
         }
+
+        needsLayout = false
     }
 
     /// Lays out a single text line.
