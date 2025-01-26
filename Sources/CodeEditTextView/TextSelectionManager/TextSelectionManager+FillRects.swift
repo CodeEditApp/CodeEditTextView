8834ce4a1832497e7e0fa96a0a384f9dc5555586
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-01-26 12:50:27 -0600

    Disambiguate Selection Rect Drawing

--- Sources/CodeEditTextView/TextSelectionManager/TextSelectionManager+FillRects.swift
+++ Sources/CodeEditTextView/TextSelectionManager/TextSelectionManager+FillRects.swift
@@ -23,38 +23,13 @@ extension TextSelectionManager {
         let range = textSelection.range
 
         var fillRects: [CGRect] = []
-        guard let firstLinePosition = layoutManager.lineStorage.getLine(atOffset: range.location),
-              let lastLinePosition = range.max == layoutManager.lineStorage.length
-                ? layoutManager.lineStorage.last
-                : layoutManager.lineStorage.getLine(atOffset: range.max) else {
-            return []
-        }
 
         let insetXPos = max(edgeInsets.left, rect.minX)
         let insetWidth = max(0, rect.maxX - insetXPos - edgeInsets.right)
         let insetRect = NSRect(x: insetXPos, y: rect.origin.y, width: insetWidth, height: rect.height)
 
-        // Calculate the first line and any rects selected
-        // If the last line position is not the same as the first, calculate any rects from that line.
-        // If there's > 0 space between the first and last positions, add a rect between them to cover any
-        // intermediate lines.
-
-        let firstLineRects = getFillRects(in: rect, selectionRange: range, forPosition: firstLinePosition)
-        let lastLineRects: [CGRect] = if lastLinePosition.range != firstLinePosition.range {
-            getFillRects(in: rect, selectionRange: range, forPosition: lastLinePosition)
-        } else {
-            []
-        }
-
-        fillRects.append(contentsOf: firstLineRects + lastLineRects)
-
-        if firstLinePosition.yPos + firstLinePosition.height < lastLinePosition.yPos {
-            fillRects.append(CGRect(
-                x: insetXPos,
-                y: firstLinePosition.yPos + firstLinePosition.height,
-                width: insetWidth,
-                height: lastLinePosition.yPos - (firstLinePosition.yPos + firstLinePosition.height)
-            ))
+        for linePosition in layoutManager.lineStorage.linesInRange(range) {
+            fillRects.append(contentsOf: getFillRects(in: insetRect, selectionRange: range, forPosition: linePosition))
         }
 
         // Pixel align these to avoid aliasing on the edges of each rect that should be a solid box.
