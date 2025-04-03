49b9a5e0e9266dd61816208bbfc6eaa3f0545f71
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-04-03 08:15:20 -0700

    Comment on line drawing

--- Sources/CodeEditTextView/TextLine/LineFragmentView.swift
+++ Sources/CodeEditTextView/TextLine/LineFragmentView.swift
@@ -42,6 +42,21 @@ final class LineFragmentView: NSView {
         }
         context.saveGState()
 
+        // Removes jagged edges
+        context.setAllowsAntialiasing(true)
+        context.setShouldAntialias(true)
+
+        // Effectively increases the screen resolution by drawing text in each LED color pixel (R, G, or B), rather than
+        // the triplet of pixels (RGB) for a regular pixel. This can increase text clarity, but loses effectiveness
+        // in low-contrast settings.
+        context.setAllowsFontSubpixelPositioning(true)
+        context.setShouldSubpixelPositionFonts(true)
+
+        // Quantizes the position of each glyph, resulting in slightly less accurate positioning, and gaining higher
+        // quality bitmaps and performance.
+        context.setAllowsFontSubpixelQuantization(true)
+        context.setShouldSubpixelQuantizeFonts(true)
+
         ContextSetHiddenSmoothingStyle(context, 16)
 
         context.textMatrix = .init(scaleX: 1, y: -1)
