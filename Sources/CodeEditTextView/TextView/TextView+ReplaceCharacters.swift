d2b53a5d8f77bc7eff835a1bb766881e46f03505
hi2gage <hi2gage.pdx@gmail.com>
2025-04-11 22:16:54 -0600

    Add skupUpdateSelection

--- Sources/CodeEditTextView/TextView/TextView+ReplaceCharacters.swift
+++ Sources/CodeEditTextView/TextView/TextView+ReplaceCharacters.swift
@@ -13,7 +13,12 @@ extension TextView {
     /// - Parameters:
     ///   - ranges: The ranges to replace
     ///   - string: The string to insert in the ranges.
-    public func replaceCharacters(in ranges: [NSRange], with string: String) {
+    ///   - skipUpdateSelection: Skips the selection update step
+    public func replaceCharacters(
+        in ranges: [NSRange],
+        with string: String,
+        skipUpdateSelection: Bool = false
+    ) {
         guard isEditable else { return }
         NotificationCenter.default.post(name: Self.textWillChangeNotification, object: self)
         textStorage.beginEditing()
@@ -34,13 +39,17 @@ extension TextView {
                 in: range,
                 with: NSAttributedString(string: string, attributes: typingAttributes)
             )
-            selectionManager.didReplaceCharacters(in: range, replacementLength: (string as NSString).length)
+            if !skipUpdateSelection {
+                selectionManager.didReplaceCharacters(in: range, replacementLength: (string as NSString).length)
+            }
 
             delegate?.textView(self, didReplaceContentsIn: range, with: string)
         }
 
         textStorage.endEditing()
-        selectionManager.notifyAfterEdit()
+        if !skipUpdateSelection {
+            selectionManager.notifyAfterEdit()
+        }
         NotificationCenter.default.post(name: Self.textDidChangeNotification, object: self)
 
         // `scrollSelectionToVisible` is a little expensive to call every time. Instead we just check if the first
@@ -54,7 +63,33 @@ extension TextView {
     /// - Parameters:
     ///   - range: The range to replace.
     ///   - string: The string to insert in the range.
-    public func replaceCharacters(in range: NSRange, with string: String) {
-        replaceCharacters(in: [range], with: string)
+    ///   - skipUpdateSelection: Skips the selection update step
+    public func replaceCharacters(
+        in range: NSRange,
+        with string: String,
+        skipUpdateSelection: Bool = false
+    ) {
+        replaceCharacters(in: [range], with: string, skipUpdateSelection: skipUpdateSelection)
+    }
+
+    /// Iterates over all text selections in the `TextView` and applies the provided callback.
+    ///
+    /// This method is typically used when you need to perform an operation on each text selection in the editor,
+    /// such as adjusting indentation, or other selection-based operations. The callback
+    /// is executed for each selection, and you can modify the selection or perform related tasks.
+    ///
+    /// - Parameters:
+    /// - callback: A closure that will be executed for each selection in the `TextView`. It takes two parameters:
+    /// a `TextView` instance, allowing access to the view's properties and methods and a
+    /// `TextSelectionManager.TextSelection` representing the current selection to operate on.
+    ///
+    /// - Note: The selections are iterated in reverse order, so modifications to earlier selections won't affect later
+    ///   ones. The method automatically calls `notifyAfterEdit()` on the `selectionManager` after all
+    ///   selections are processed.
+    public func editSelections(callback: (TextView, TextSelectionManager.TextSelection) -> Void) {
+        for textSelection in selectionManager.textSelections.reversed() {
+            callback(self, textSelection)
+        }
+        selectionManager.notifyAfterEdit(force: true)
     }
 }
