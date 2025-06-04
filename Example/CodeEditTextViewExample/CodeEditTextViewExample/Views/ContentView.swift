5c0ea184e9a5d14e3e213200b7cb73860b2b5b17
Austin Condiff <austin.condiff@gmail.com>
2025-06-04 00:00:26 -0500

    Added product icon and status bar to example app similar to what has been done to CodeEditSourceEditor.

--- Example/CodeEditTextViewExample/CodeEditTextViewExample/Views/ContentView.swift
+++ Example/CodeEditTextViewExample/CodeEditTextViewExample/Views/ContentView.swift
@@ -11,18 +11,28 @@ struct ContentView: View {
     @Binding var document: CodeEditTextViewExampleDocument
     @AppStorage("wraplines") private var wrapLines: Bool = true
     @AppStorage("edgeinsets") private var enableEdgeInsets: Bool = false
+    @AppStorage("usesystemcursor") private var useSystemCursor: Bool = false
+    @AppStorage("isselectable") private var isSelectable: Bool = true
+    @AppStorage("iseditable") private var isEditable: Bool = true
 
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Toggle("Wrap Lines", isOn: $wrapLines)
-                Toggle("Inset Edges", isOn: $enableEdgeInsets)
-            }
-            Divider()
-            SwiftUITextView(
+        SwiftUITextView(
+            text: document.text,
+            wrapLines: $wrapLines,
+            enableEdgeInsets: $enableEdgeInsets,
+            useSystemCursor: $useSystemCursor,
+            isSelectable: $isSelectable,
+            isEditable: $isEditable
+        )
+        .padding(.bottom, 28)
+        .overlay(alignment: .bottom) {
+            StatusBar(
                 text: document.text,
                 wrapLines: $wrapLines,
-                enableEdgeInsets: $enableEdgeInsets
+                enableEdgeInsets: $enableEdgeInsets,
+                useSystemCursor: $useSystemCursor,
+                isSelectable: $isSelectable,
+                isEditable: $isEditable
             )
         }
     }
--- Example/CodeEditTextViewExample/CodeEditTextViewExample/Views/SwiftUITextView.swift
+++ Example/CodeEditTextViewExample/CodeEditTextViewExample/Views/SwiftUITextView.swift
@@ -13,17 +13,26 @@ struct SwiftUITextView: NSViewControllerRepresentable {
     var text: NSTextStorage
     @Binding var wrapLines: Bool
     @Binding var enableEdgeInsets: Bool
+    @Binding var useSystemCursor: Bool
+    @Binding var isSelectable: Bool
+    @Binding var isEditable: Bool
 
     func makeNSViewController(context: Context) -> TextViewController {
         let controller = TextViewController(string: "")
         controller.textView.setTextStorage(text)
         controller.wrapLines = wrapLines
         controller.enableEdgeInsets = enableEdgeInsets
+        controller.useSystemCursor = useSystemCursor
+        controller.isSelectable = isSelectable
+        controller.isEditable = isEditable
         return controller
     }
 
     func updateNSViewController(_ nsViewController: TextViewController, context: Context) {
         nsViewController.wrapLines = wrapLines
         nsViewController.enableEdgeInsets = enableEdgeInsets
+        nsViewController.useSystemCursor = useSystemCursor
+        nsViewController.isSelectable = isSelectable
+        nsViewController.isEditable = isEditable
     }
 }
